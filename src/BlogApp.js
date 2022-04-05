873de0be1691995b2a3bd6c0a4cc44864d16d30c
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-04-05 14:42:32 +0300

    Finished exercise 5.3
    bloglist frontend, step3

--- src/BlogApp.js
+++ src/BlogApp.js
@@ -3,60 +3,139 @@ import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 const BlogApp = () => {
+  // States
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-   const handleLogin = async (event) => {
-     event.preventDefault();
-     try {
-       const user = await loginService.login({
-         username,
-         password,
-       });
-       window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
-       setUser(user);
-       setUsername("");
-       setPassword("");
-     } catch (exception) {
-       console.log(exception);
-     }
-   };
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
 
-   
+  // Handlers
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+      setUser(user);
+      blogService.setToken(user.token);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.log(exception);
+    }
+  };
+
+  const addNewBlog = async (event) => {
+    event.preventDefault();
+    const blogObject = {
+      title,
+      author,
+      url,
+    };
+    try {
+      const returnedBlog = await blogService.create(blogObject);
+      setBlogs(blogs.concat(returnedBlog));
+      setTitle("");
+      setAuthor("");
+      setUrl("");
+    } catch (exception) {
+      console.log(exception);
+    }
+  };
+  // Effects Hooks
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
-  
   const loginForm = () => (
     <>
       <h2>Log in to application</h2>
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
     </>
   );
+
+  const BlogForm = ({ addNewBlog, title, author, url }) => {
+    return (
+      <>
+        <h2>Create new blog</h2>
+        <form onSubmit={addNewBlog}>
+          <div>
+            title
+            <input
+              type="text"
+              value={title}
+              name="Title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            author
+            <input
+              type="text"
+              value={author}
+              name="Author"
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            url
+            <input
+              type="text"
+              value={url}
+              name="Url"
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </div>
+          <button type="submit">create</button>
+        </form>
+      </>
+    );
+  };
+
+  const App = ({ blogs, addNewBlog, title, url, author }) => {
+    return (
+      <>
+        <BlogForm
+          addNewBlog={addNewBlog}
+          title={title}
+          url={url}
+          author={author}
+        />
+
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </>
+    );
+  };
+
   return (
     <div>
       {user === null ? null : (
@@ -66,9 +145,9 @@ const BlogApp = () => {
             {user.name} logged in{" "}
             <button
               type="reset"
-              onClick={() =>{
-                window.localStorage.removeItem("loggedNoteappUser")
-                setUser(null)
+              onClick={() => {
+                window.localStorage.removeItem("loggedNoteappUser");
+                setUser(null);
               }}
             >
               logout
@@ -76,9 +155,17 @@ const BlogApp = () => {
           </h4>
         </div>
       )}
-      {user === null
-        ? loginForm()
-        : blogs.map((blog) => <Blog key={blog.id} blog={blog} />)}
+      {user === null ? (
+        loginForm()
+      ) : (
+        <App
+          blogs={blogs}
+          addNewBlog={addNewBlog}
+          title={title}
+          url={url}
+          author={author}
+        />
+      )}
       {}
     </div>
   );
